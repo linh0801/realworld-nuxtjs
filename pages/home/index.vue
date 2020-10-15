e169811d7f5a2a4ce893e9d028eaed8f2f2f2841
Administrator <571187984@qq.com>
2020-10-15 17:50:35 +0800

    update index

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -3,7 +3,7 @@
 
   <div class="banner">
     <div class="container">
-      <h1 class="logo-font">王者荣耀V5</h1>
+      <h1 class="logo-font">王者荣耀,农药</h1>
       <p>A place to share your knowledge.</p>
     </div>
   </div>
